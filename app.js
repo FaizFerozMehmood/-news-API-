7e020d29a4951fa10ff70013978ddaae494b884a
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-06-28 15:13:31 +0500

    changes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let getDiv = document.getElementById('getDiv');
+let INPUt = document.getElementById('inpuT')
+let btn = document.getElementById('btn')
+btn.addEventListener('click',function(){
 
-fetch(`https://newsapi.org/v2/everything?q=karachi&from=2024-05-27&sortBy=publishedAt&apiKey=78eeb81244cd4d79a1ab6bf2ed4d089f`)
+  fetch(`https://newsapi.org/v2/everything?q=${INPUt.value}&from=2024-05-27&sortBy=publishedAt&apiKey=78eeb81244cd4d79a1ab6bf2ed4d089f`)
   .then(response => response.json())
   .then(data => {
     for (let i = 0; i < data.articles.length; i++) {
@@ -9,7 +12,7 @@ fetch(`https://newsapi.org/v2/everything?q=karachi&from=2024-05-27&sortBy=publis
       getDiv.innerHTML += `
         <div class="card" style="width: 18rem;">
           <img src="${article.urlToImage}" class="card-img-top" alt="${article.title}">
-          <div class="card-body">
+          <div class="card-body ">
             <h5 class="card-title">${article.title}</h5>
             <p class="card-text">${article.description}</p>
             <a href="${article.url}" class="btn btn-primary" target="_blank">Read more</a>
@@ -18,3 +21,8 @@ fetch(`https://newsapi.org/v2/everything?q=karachi&from=2024-05-27&sortBy=publis
     }
   })
   .catch(err => console.log(err));
+
+
+})
+
+
